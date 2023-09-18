3f2d54c6da56bc74813c9c981453ccd3fed08685
aleung910 <aliceleung34@gmail.com>
2023-09-17 22:26:09 -0400

    Day 2 of CW

--- JS/main.js
+++ JS/main.js
@@ -40,3 +40,31 @@ function getPlanetName(id){
     
     return name;
   }
+
+  //day2 codewars
+
+  function checkAlive (health) {
+    if ( health <= 0) {
+      return false
+    } 
+    else {
+      return true
+    }
+  }
+//removing first and last char
+  function removeChar(str){
+    return str.slice(1,-1)
+   };
+
+//removing everyother element
+function removeEveryOther(arr){
+  return arr.filter((element, index) => index%2===0 )
+}
+
+//returning negatives
+function makeNegative(num) {
+  return num<0 ? num : num *-1;
+  }
+  
+//if already neg return the way it is, if postive return
+//negative of itself 
