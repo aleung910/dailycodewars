0050d3ec39d9be08a2b42e826c4db69a9dab6980
aleung910 <aliceleung34@gmail.com>
2023-09-22 19:37:41 -0400

    Day 7 of CW

--- JS/main.js
+++ JS/main.js
@@ -167,3 +167,17 @@ function grow(x){
 function twiceAsOld(dadYearsOld, sonYearsOld) {
   return Math.abs(dadYearsOld - 2 * sonYearsOld);
 }
+
+function solution(number){
+  if (number < 0){
+    return 0;
+  }
+  let sum=0;
+  for (let i=0; i < number; i++){
+    if (i%3 === 0 || i%5 === 0){
+      sum += i;
+    }
+  }
+  return sum;
+
+}
