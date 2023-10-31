7cda9bf8d8b90aaa9def0afd477018492451ed7b
aleung910 <aliceleung34@gmail.com>
2023-10-30 21:37:04 -0400

    Day 35 of CW

--- JS/main.js
+++ JS/main.js
@@ -442,3 +442,13 @@ function isLeapYear(year) {
     return false;
   }
 }
+//day35 of cw
+function elevatorDistance(array) {
+  let totalDistance = 0;
+  
+  for (let i = 0; i < array.length - 1; i++) {
+    totalDistance += Math.abs(array[i] - array[i + 1]);
+  }
+  
+  return totalDistance;
+}
